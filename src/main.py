bc045a4cdc67457ae094e136712947a960bee49d
mfpachecog <manuelfpg012@gmail.com>
2025-11-23 22:14:57 -0500

    configure main.py to add jobs.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
 from src.services.lifecycle import lifespan
-from src.api.routers import candidates # importar jobs tambien
-# from src.api.routers import jobs 
+from src.api.routers import candidates, jobs # importar jobs tambien
+
 
 app = FastAPI(
     title="AI CV Analyzer",
+    version = "1.0.0",
     lifespan=lifespan
 )
 
 app.include_router(candidates.router, prefix="/candidates", tags=["Candidatos"])
-# app.include_router(jobs.router, prefix="/jobs", tags=["Ofertas"])
+app.include_router(jobs.router, prefix="/jobs", tags=["Ofertas"])
 
 @app.get("/")
 async def root():
